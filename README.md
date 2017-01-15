f2e40f60dd9349087e367346beb1d28421e18bee
spacedragon <spacedragon@me.com>
2017-01-15 10:58:25 +0800

    加入readme 和注释

--- README.md
+++ README.md
@@ -1,5 +1,27 @@
-# 微信小程序示例项目
+## What
+基于 [Labrador](https://github.com/maichong/labrador) 和 [mobx](https://mobx.js.org/index.html) 框架构建的小程序开发demo。
 
-基于 [Labrador](https://github.com/maichong/labrador) 框架构建。
+## Why 
+Labrador 开发框架使微信开发回到我们熟悉的开发环境和流程中来 ,  更多特性请参考 [Labrador](https://github.com/maichong/labrador)
+使用Mobx 来做状态管理相比 Redux 来说更简单易懂, 更自由,  代码更少.
 
- 
+## How  
+
+```
+	git clone https://github.com/spacedragon/labrador_mobx_example.git
+	npm install
+	npm run dev
+```
+
+## So ...
+熟悉mobx 的看 hocs/observer.js  里面的注释
+
+## Know Issues
+1. Labrador 可能存在 [issue](https://github.com/maichong/labrador/issues/35)  所以目前package.json 里面使用的是我 fork 之后修改过的版本
+
+2. 微信小程序的 Page.setData(data)  对data 做了一次拷贝, 但是拷贝出来的对象丢失了 getter 方法, 这使得在 xml/wxml 界面绑定的时候取不到 mobx computed value ,  目前的解决方法是为 store 写一个 toJS 序列化方法, 将 computed value 转化为普通的 property .  (同样需要修改 labrador 框架)
+3. 目前使用mobx 2.7.0  , 目测 3.0.0  的版本在小程序环境中存在兼容性问题, 需要进一步研究.
+4. mobx 有可能比  redux / saga  更占空间 ,  注意小程序有 1m 的限制.  (mobx.min.js 有51k肯定比 redux 大许多, 但是因为mobx 需要的业务逻辑代码更少, 也许在更大的项目中会有优势)
+
+
+
--- src/hocs/observer.js
+++ src/hocs/observer.js
@@ -1,54 +1,68 @@
 import { autorun, observable, action } from 'mobx';
 import * as utils from 'labrador/utils';
 import pick from 'lodash/pick';
+// 我们所有的 store 都在这里
 import stores from '../stores/index';
 
+/**
+ * 让小程序控件动起来的High Order Component (高阶控件)
+ * 最后一个参数是 component , 前面的都是需要 inject 的store 名称
+ */
 export default function observer(...args) {
   let storeNames = args.slice();
   let component = storeNames.pop();
 
+  // labrador 定义的2个lifecycle 函数
   let onLoad: Function = component.prototype.onLoad;
   let onUnload: Function = component.prototype.onUnload;
+
+  // 开关,  onLoad的时候打开 ,  onUnLoad 的时候关闭
   let connected = observable(false);
-  function onStateChange() {
+  function onStateChange() { // 从 labrador-redux 中学来的函数
     if (this.onUpdate) {
       this.onUpdate();
       if (__DEV__) {
-          // Development
+        // Development
         console.log('%c%s onUpdate(%o) Component:%o',
-            'color:#2a8f99',
-            this.id, utils.getDebugObject(this.props),
-            this
-          );
+          'color:#2a8f99',
+          this.id, utils.getDebugObject(this.props),
+          this
+        );
       }
     }
-    this._update();
+    this._update(); // 触发控件相关的更新, 在这个函数中会读取 那些 mobx observable
   }
 
-  component.prototype.onLoad = function (...args1) {
+  component.prototype.onLoad = function (...args1) { // 控件被加载
+    // 在autorun onStateChange , autorun 会捕捉所有相关的observable
+    // todo: autorun 会不会太糙?  用mobx.Reaction 是不是可以更精细控制?
     autorun(() => {
       if (connected.get()) {
         onStateChange.apply(this);
       }
     });
-    action(() => connected.set(true))();
+    action(() => connected.set(true))();     // 打开我们的开关 , 让组件动起来
+
     if (onLoad) {
       onLoad.apply(this, args1);
     }
   };
 
   component.prototype.onUnload = function () {
-    if (connected) {
-      action(() => connected.set(false))();
+    if (connected.get()) {
+      action(() => connected.set(false))();  //组件被卸载, 关闭
     }
     if (onUnload) {
       onUnload.call(this);
     }
   };
 
+  // 或许应该在 OnShow OnHide 中加入开关 ,  因为页面隐藏的时候也没必要触发渲染
+
+
   let extend = function (cls) {
     function wrappedClass(props) {
-       // 注入 store
+      // 注入 store , 见 lodash 的pick 函数
       let propsWithStore = { ...props, ...pick(stores, ...storeNames) };
       cls.apply(this, [propsWithStore]);
     }
